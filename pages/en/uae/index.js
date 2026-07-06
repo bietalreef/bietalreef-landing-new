4fd9e3cf182a0d846d714783a3b49aac7f3ba333
bietalreef <bietalreef@gmail.com>
2026-07-06 22:06:12 +0400

    Add smart UAE footer to English UAE index

--- pages/en/uae/index.js
+++ pages/en/uae/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import EnglishLayout from '../../../components/EnglishLayout';
+import UaeSmartFooter from '../../../components/UaeSmartFooter';
 import { UAE_EMIRATES } from '../../../data/siteTaxonomy';
 
 export default function EnglishUaeIndex() {
@@ -28,6 +29,7 @@ export default function EnglishUaeIndex() {
             ))}
           </div>
         </main>
+        <UaeSmartFooter locale="en" pageType="index" />
       </EnglishLayout>
     </>
   );
