86a1f023f299d64322265390163858e90f4fe079
bietalreef <bietalreef@gmail.com>
2026-07-07 10:10:36 +0400

    Add English partners footer page

---
+++ pages/en/partners.js
@@ -0,0 +1,13 @@
+import EnglishGenericPage from '../../components/EnglishGenericPage';
+
+export default function PartnersEnglishPage() {
+  return (
+    <EnglishGenericPage
+      title="Partners"
+      description="Biet Al Reef is building a partner ecosystem for service providers, suppliers, factories and organizations that support construction and maintenance projects in the UAE."
+      path="/en/partners"
+      ctaHref="/en/contact"
+      ctaLabel="Contact partnerships"
+    />
+  );
+}
