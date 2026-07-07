b74892a36f12c1b429c85a846b5ea485cb8df8fe
bietalreef <bietalreef@gmail.com>
2026-07-07 10:10:02 +0400

    Add English marketplace footer page

---
+++ pages/en/marketplace.js
@@ -0,0 +1,13 @@
+import EnglishGenericPage from '../../components/EnglishGenericPage';
+
+export default function MarketplaceEnglishPage() {
+  return (
+    <EnglishGenericPage
+      title="Products & Stores"
+      description="A future marketplace path for building materials, finishing products, smart home systems, suppliers and factories connected to Biet Al Reef."
+      path="/en/marketplace"
+      ctaHref="/en/contact"
+      ctaLabel="Ask about products"
+    />
+  );
+}
