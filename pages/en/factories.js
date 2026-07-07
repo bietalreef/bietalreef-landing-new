c35c23c1095d32aefc01dcffb985ae466c5cdff0
bietalreef <bietalreef@gmail.com>
2026-07-07 10:11:00 +0400

    Add English factories footer page

---
+++ pages/en/factories.js
@@ -0,0 +1,13 @@
+import EnglishGenericPage from '../../components/EnglishGenericPage';
+
+export default function FactoriesEnglishPage() {
+  return (
+    <EnglishGenericPage
+      title="Factories"
+      description="A future directory path for factories and manufacturers that support construction, finishing, furniture, interior work and building material supply in the UAE."
+      path="/en/factories"
+      ctaHref="/en/providers/register"
+      ctaLabel="Register factory interest"
+    />
+  );
+}
