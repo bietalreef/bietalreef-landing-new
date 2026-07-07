09e889fffb4a824ef82a526fc177dd6cc230d812
bietalreef <bietalreef@gmail.com>
2026-07-07 10:10:26 +0400

    Add English blog footer page

---
+++ pages/en/blog.js
@@ -0,0 +1,13 @@
+import EnglishGenericPage from '../../components/EnglishGenericPage';
+
+export default function BlogEnglishPage() {
+  return (
+    <EnglishGenericPage
+      title="Blog"
+      description="Insights and future articles about construction, maintenance, digital presence, UAE service discovery and Biet Al Reef platform updates."
+      path="/en/blog"
+      ctaHref="/en/uae"
+      ctaLabel="Explore UAE Directory"
+    />
+  );
+}
