40886080abc4492b575988fdad8c14d8c3aed2f2
bietalreef <bietalreef@gmail.com>
2026-07-08 23:19:10 +0400

    Add global favicon and app identity metadata

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,15 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="ar" dir="rtl">
-      <Head />
+      <Head>
+        <link rel="icon" href="/logo.png" type="image/png" />
+        <link rel="shortcut icon" href="/logo.png" type="image/png" />
+        <link rel="apple-touch-icon" href="/logo.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="application-name" content="بيت الريف | Biet Al Reef" />
+        <meta name="apple-mobile-web-app-title" content="بيت الريف" />
+        <meta name="theme-color" content="#0F3F1A" />
+      </Head>
       <body>
         <Main />
         <NextScript />
