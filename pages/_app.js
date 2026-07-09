4ee7c0a2751fe76a52a0009d28500c6338319a0e
bietalreef <bietalreef@gmail.com>
2026-07-09 19:34:02 +0400

    Add universal CTA fallback for English pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,17 @@
 import "../styles/globals.css";
 import "../styles/provider_styles.css";
 import { useEffect } from "react";
+import { useRouter } from "next/router";
 import InstallPrompt from "../components/InstallPrompt";
 import AppOpenGate from "../components/AppOpenGate";
 import WeyakChat from "../components/WeyakChat";
+import UniversalRequestCTA from "../components/UniversalRequestCTA";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  const isEnglishPage = router.asPath?.split('?')[0]?.startsWith('/en');
+
   useEffect(() => {
     // Register service worker for PWA
     if (typeof window !== "undefined" && "serviceWorker" in navigator) {
@@ -22,6 +27,7 @@ export default function MyApp({ Component, pageProps }) {
       <AppOpenGate />
       <WeyakChat />
       <Component {...pageProps} />
+      {isEnglishPage ? <UniversalRequestCTA locale="en" /> : null}
       <SpeedInsights />
     </>
   );
