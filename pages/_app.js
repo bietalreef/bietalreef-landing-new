3fc87883724de5e264f60c2c00d75a9ad22c700b
bietalreef <bietalreef@gmail.com>
2026-07-13 13:40:53 +0400

    Remove incompatible Speed Insights component causing runtime crash

--- pages/_app.js
+++ pages/_app.js
@@ -6,14 +6,12 @@ import InstallPrompt from "../components/InstallPrompt";
 import AppOpenGate from "../components/AppOpenGate";
 import WeyakChat from "../components/WeyakChat";
 import UniversalRequestCTA from "../components/UniversalRequestCTA";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function MyApp({ Component, pageProps }) {
   const router = useRouter();
   const isEnglishPage = router.asPath?.split('?')[0]?.startsWith('/en');
 
   useEffect(() => {
-    // Register service worker for PWA
     if (typeof window !== "undefined" && "serviceWorker" in navigator) {
       navigator.serviceWorker.register("/sw.js").catch((error) => {
         console.log("Service Worker registration failed:", error);
@@ -28,7 +26,6 @@ export default function MyApp({ Component, pageProps }) {
       <WeyakChat />
       <Component {...pageProps} />
       {isEnglishPage ? <UniversalRequestCTA locale="en" /> : null}
-      <SpeedInsights />
     </>
   );
 }
