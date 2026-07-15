a19c3e15d6575aa3425d5d521deee26485d03fbe
bietalreef <bietalreef@gmail.com>
2026-07-15 14:15:29 +0400

    perf: defer provider-only CSS and JavaScript

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,8 @@
 import "../styles/globals.css";
-import "../styles/provider_styles.css";
-import "../styles/provider_product_cards.css";
-import "../styles/arkleen_provider_preview.css";
 import { useRouter } from "next/router";
+import Head from "next/head";
+import dynamic from "next/dynamic";
 import ClientSafetyBoundary from "../components/ClientSafetyBoundary";
-import ProviderProductInteraction from "../components/provider/ProviderProductInteraction";
-import ArklineProfileEnhancements from "../components/provider/ArklineProfileEnhancements";
-import ArklineProjectsAndChannels from "../components/provider/ArklineProjectsAndChannels";
 import PrivacyConsentCenter from "../components/PrivacyConsentCenter";
 import ConsentAwareSpeedInsights from "../components/ConsentAwareSpeedInsights";
 import PwaLifecycleManager from "../components/PwaLifecycleManager";
@@ -14,19 +10,45 @@ import PwaInstallPrompt from "../components/PwaInstallPrompt";
 import { initPublicAnalytics } from "../lib/publicAnalytics";
 import { useEffect } from "react";
 
+const ProviderProductInteraction = dynamic(
+  () => import("../components/provider/ProviderProductInteraction"),
+  { ssr: false }
+);
+const ArklineProfileEnhancements = dynamic(
+  () => import("../components/provider/ArklineProfileEnhancements"),
+  { ssr: false }
+);
+const ArklineProjectsAndChannels = dynamic(
+  () => import("../components/provider/ArklineProjectsAndChannels"),
+  { ssr: false }
+);
+
 export default function MyApp({ Component, pageProps }) {
   const router = useRouter();
   const currentPath = router.asPath?.split('?')[0] || '/';
   const isEnglishPage = currentPath === '/en' || currentPath.startsWith('/en/');
+  const isProviderProfile = /^\/(?:en\/)?providers\/(?!register(?:\/|$))[^/?#]+/.test(currentPath);
+  const isArkleenProfile = /^\/(?:en\/)?providers\/(?:arkleen|arkline)(?:\/|$)/.test(currentPath);
 
   useEffect(() => initPublicAnalytics(router), [router]);
 
   return (
     <ClientSafetyBoundary>
       <Component {...pageProps} />
-      <ProviderProductInteraction currentPath={router.asPath || ''} />
-      <ArklineProfileEnhancements currentPath={router.asPath || ''} />
-      <ArklineProjectsAndChannels currentPath={router.asPath || ''} />
+      {isProviderProfile ? (
+        <>
+          <Head>
+            <link rel="stylesheet" href="/styles/provider-profile.css" />
+          </Head>
+          <ProviderProductInteraction currentPath={router.asPath || ''} />
+        </>
+      ) : null}
+      {isArkleenProfile ? (
+        <>
+          <ArklineProfileEnhancements currentPath={router.asPath || ''} />
+          <ArklineProjectsAndChannels currentPath={router.asPath || ''} />
+        </>
+      ) : null}
       <PrivacyConsentCenter locale={isEnglishPage ? "en" : "ar"} />
       <ConsentAwareSpeedInsights />
       <PwaInstallPrompt locale={isEnglishPage ? "en" : "ar"} />
