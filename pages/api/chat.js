197191aba20cf1a4cd6502d87ec9d8262e3b1b0a
bietalreef <bietalreef@gmail.com>
2026-07-16 03:39:16 +0400

    Use minimal reasoning for stage-one Weyaak requests

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -1,1 +1,8 @@
-export { default } from '../../lib/weyaakAgentHandlerV5';
+export default async function handler(req, res) {
+  if (!process.env.WEYAAK_REASONING_EFFORT) {
+    process.env.WEYAAK_REASONING_EFFORT = 'minimal';
+  }
+
+  const { default: weyaakAgentHandler } = await import('../../lib/weyaakAgentHandlerV5');
+  return weyaakAgentHandler(req, res);
+}
