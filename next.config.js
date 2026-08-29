5b9af3fc4a92518976f4756bb0d8b66d8c004a37
bietalreef <bietalreef@gmail.com>
2026-08-29 12:09:44 +0400

    Serve local images directly to avoid Next image optimizer crawl errors

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: 'https', hostname: 'i.ytimg.com', pathname: '/vi/**' },
     ],
@@ -28,6 +29,10 @@ const nextConfig = {
       },
       {
         source: '/images/:path*',
+        headers: [{ key: 'Cache-Control', value: 'public, max-age=31536000, immutable' }],
+      },
+      {
+        source: '/icons/:path*',
         headers: [{ key: 'Cache-Control', value: 'public, max-age=31536000, immutable' }],
       },
       {
